a608717acbce8e9c121978bf90a18a383553989e
leo.devic <4631057@studenti.unige.it>
2023-11-23 16:05:04 +0100

    aggiunto test parametrizzato sulla somma

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -4,6 +4,8 @@ import code.Business_logic.Euro;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestEuro {
     private Euro euro;
@@ -30,5 +32,13 @@ public class TestEuro {
         Euro otherEuro = new Euro(3);
         Euro result = euro.sottrai(otherEuro);
         Assertions.assertEquals(7, result.getValore());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    public void testParameterized(int value) {
+        Euro otherEuro = new Euro(value);
+        Euro result = euro.somma(otherEuro);
+        Assertions.assertEquals(10 + value, result.getValore());
     }
 }
