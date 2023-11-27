984f7df895d48db1cd1c3d1b8d6d435eb01120f4
s5003659 <traversa.riccardo14@gmail.com>
2023-11-27 20:42:15 +0100

    Refactor TestEuro.java in package test

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -4,8 +4,6 @@ import code.Business_logic.Euro;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.*;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestEuro {
     private Euro euro;
@@ -16,29 +14,42 @@ public class TestEuro {
     }
     
     @Test
-    public void testGetAmount() {
-        Assertions.assertEquals(10, euro.getValore());
+    public void testGetValore() {
+        Assertions.assertEquals(1000, euro.getValore());
     }
     
     @Test
-    public void testAdd() {
+    public void testSomma() {
         Euro otherEuro = new Euro(5);
         Euro result = euro.somma(otherEuro);
-        Assertions.assertEquals(15, result.getValore());
+        Assertions.assertEquals(1500, result.getValore());
     }
     
     @Test
-    public void testSubtract() {
+    public void testSottrai() {
         Euro otherEuro = new Euro(3);
         Euro result = euro.sottrai(otherEuro);
-        Assertions.assertEquals(7, result.getValore());
+        Assertions.assertEquals(700, result.getValore());
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5})
-    public void testParameterized(int value) {
-        Euro otherEuro = new Euro(value);
-        Euro result = euro.somma(otherEuro);
-        Assertions.assertEquals(10 + value, result.getValore());
+    
+    @Test
+    public void testUgualeA() {
+        Euro otherEuro = new Euro(10);
+        Assertions.assertTrue(euro.ugualeA(otherEuro));
     }
+
+    @Test
+
+    public void testMinoreDi() {
+        Euro otherEuro = new Euro(20);
+        Assertions.assertTrue(euro.minoreDi(otherEuro));
+    }
+
+    @Test
+    public void testStampa() {
+        Assertions.assertEquals("10.0 euro", euro.stampa());
+    }
+
+    
 }
